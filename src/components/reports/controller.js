2debba20bc8c6824cbf8de115bb2388f0c42ab5f
thinhngoo <ncathinh21@clc.fitus.edu.vn>
2024-01-05 22:15:21 +0700

    Update chart service

--- src/components/reports/controller.js
+++ src/components/reports/controller.js
@@ -1,10 +1,11 @@
 const reportService = require("./service");
 
 exports.displayCharts = async (req, res, next) => {
-    const chartData = await reportService.getProductRevenue(2, 10);
+    const chartData = await reportService.getTotalRevenues(3);
     res.render("reports/charts", {
         data: JSON.stringify(chartData),
         name: "Statistics",
         size: "800px",
+        type: "bar",
     });
 }
